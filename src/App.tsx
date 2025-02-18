2153e58e69606d665b2ca7ae215acdfd953a157c
gponciano <gab_bash@hotmail.com>
2025-02-19 06:47:52 +1100

    Adding Button, Input, Letter and Tip components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
 
+import './global.css';
 import styles from './App.module.css';
 import { useState } from "react";
 
@@ -15,6 +20,23 @@ export function App(){
     <div className={styles.container}>
       <main>
         <Header current={attempt} max={10} onRestart={handleRestartGame}/>
+
+        <Tip tip={'Library to create components and interfaces'}/>
+
+        <div className={styles.word}>
+        <Letter value='R'></Letter>
+        <Letter value='R'></Letter>
+        <Letter value='R'></Letter>
+        <Letter value='R'></Letter>
+        <Letter value='R'></Letter>
+        </div>
+
+        <h4>Guess</h4>
+
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder='?'/>
+          <Button title='Confirm'/>
+        </div>
       </main>
     </div>
   )
